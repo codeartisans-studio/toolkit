582b48d77ed9fdbd7019a88848c21d4d3e2e7ad0
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-09-21 22:00:03 +0800

    update persistentutil

--- Scripts/Utils/PersistentUtil.cs
+++ Scripts/Utils/PersistentUtil.cs
@@ -2,37 +2,67 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace Toolkit.Utils
 {
 	public static class PersistentUtil
 	{
-		public static object Load (string filename)
+		public static T LoadBinary<T> (string filename) where T : class
 		{
 			string filePath = Application.persistentDataPath + "/" + filename;
 
-			object data = null;
+			T data = null;
 
 			if (File.Exists (filePath)) {
-				BinaryFormatter bf = new BinaryFormatter ();
-				FileStream file = File.Open (filePath, FileMode.Open);
-				data = bf.Deserialize (file);
-				file.Close ();
+				BinaryFormatter formatter = new BinaryFormatter ();
+
+				using (FileStream file = File.Open (filePath, FileMode.Open)) {
+					data = (T)formatter.Deserialize (file);
+				}
 			}
 
 			return data;
 		}
 
-		public static void Save (string filename, object data)
+		public static void SaveBinary<T> (string filename, T data) where T : class
 		{
 			string filePath = Application.persistentDataPath + "/" + filename;
 
-			BinaryFormatter bf = new BinaryFormatter ();
-			//Application.persistentDataPath is a string, so if you wanted you can put that into debug.log if you want to know where save games are located
-			FileStream file = File.Create (filePath); //you can call it anything you want
-			bf.Serialize (file, data);
-			file.Close ();
+			BinaryFormatter formatter = new BinaryFormatter ();
+
+			using (FileStream file = File.Create (filePath)) {
+				formatter.Serialize (file, data);
+			}
+		}
+
+		public static T LoadXml<T> (string filename) where T : class
+		{
+			string filePath = Application.persistentDataPath + "/" + filename;
+
+			T data = null;
+
+			if (File.Exists (filePath)) {
+				XmlSerializer serializer = new XmlSerializer (typeof(T));
+
+				using (FileStream file = File.Open (filePath, FileMode.Open)) {
+					data = (T)serializer.Deserialize (file);
+				}
+			}
+
+			return data;
+		}
+
+		public static void SaveXml<T> (string filename, T data) where T : class
+		{
+			string filePath = Application.persistentDataPath + "/" + filename;
+
+			XmlSerializer serializer = new XmlSerializer (typeof(T));
+
+			using (FileStream file = File.Create (filePath)) {
+				serializer.Serialize (file, data);
+			}
 		}
 	}
 }
