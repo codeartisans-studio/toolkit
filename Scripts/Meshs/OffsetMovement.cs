5d1c0dd792a8c07440e48d32dc0ef4c8e8c6ab21
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-10-12 14:05:19 +0800

    add offsetMovement script

---
+++ Scripts/Meshs/OffsetMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toolkit.Meshs
+{
+	public class OffsetMovement : MonoBehaviour
+	{
+		public Vector2 speed = Vector2.left;
+
+		private MeshRenderer mashRenderer;
+
+		void Awake ()
+		{
+			mashRenderer = GetComponent<MeshRenderer> ();
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+			Vector2 offset = -speed * Time.time;
+			mashRenderer.material.SetTextureOffset ("_MainTex", offset);
+		}
+	}
+}
