ebca147622863abba4231b9af40461c7b919565d
Kevin <kevinvqin@gmail.com>
2019-06-05 15:55:59 +0800

    add event dispatcher file

---
+++ Scripts/Event/EventDispatcher.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toolkit.Managers
+{
+    public static class EventDispatcher
+    {
+        
+    }
+}
