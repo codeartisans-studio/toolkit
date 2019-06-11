c47979dfdcb768b38517ee0a8b458c20a775f8c4
Kevin <kevinvqin@gmail.com>
2019-06-11 11:30:21 +0800

    add window manager

---
+++ Scripts/Managers/WindowsManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Toolkit.UI;
+
+namespace Toolkit.Managers
+{
+    public static class WindowsManager
+    {
+        private static List<GameObject> boards = new List<GameObject>();
+
+        public static void Add(GameObject board)
+        {
+            boards.Add(board);
+        }
+
+        public static void Remove(GameObject board)
+        {
+            boards.Remove(board);
+        }
+
+        public static void Open(GameObject board)
+        {
+            board.SetActive(true);
+        }
+
+        public static void Close(GameObject board)
+        {
+            board.SetActive(false);
+        }
+
+        public static void CloseAll()
+        {
+            foreach (GameObject board in boards)
+            {
+                Close(board);
+            }
+        }
+    }
+}
---
+++ Scripts/UI/SingletonWindow.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Toolkit.Managers;
+
+namespace Toolkit.UI
+{
+    [RequireComponent(typeof(Canvas))]
+    public class SingletonWindow<T> : Singleton<T> where T : SingletonWindow<T>
+    {
+        public GameObject board;
+
+        protected virtual void Awake()
+        {
+            Close();
+        }
+
+        protected virtual void OnEnable()
+        {
+            WindowsManager.Add(board);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            WindowsManager.Remove(board);
+        }
+
+        public void Open(bool isCloseOthers = false)
+        {
+            if (isCloseOthers)
+                WindowsManager.CloseAll();
+
+            WindowsManager.Open(board);
+        }
+
+        public void Close()
+        {
+            WindowsManager.Close(board);
+        }
+    }
+}
