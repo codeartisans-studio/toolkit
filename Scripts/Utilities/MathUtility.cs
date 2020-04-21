bf479f749925173659a83f7cc041e07dc6c65f90
Kevin <kevinvqin@gmail.com>
2020-04-21 17:20:37 +0800

    update math utility

--- Scripts/Utilities/MathUtility.cs
+++ Scripts/Utilities/MathUtility.cs
@@ -7,14 +7,10 @@ namespace Toolkit
 {
     public static class MathUtility
     {
-        // 从数组中随机出一定数量的索引
+        // 从数组中随机出一定数量的索引，如果需要数量大于数组数量，重新生成数组再次随机
         public static int[] RandomIndices(int count, int arrayCount)
         {
             List<int> indices = Enumerable.Range(0, arrayCount).ToList();
-
-            // 如果随机数量大于数组数量，直接返回全部数组
-            if (count >= arrayCount)
-                return indices.ToArray();
 
             int[] randomIndices = new int[count];
 
@@ -22,6 +18,9 @@ namespace Toolkit
             {
                 randomIndices[i] = indices[Random.Range(0, indices.Count)];
                 indices.Remove(randomIndices[i]);
+
+                if (indices.Count == 0)
+                    indices.AddRange(Enumerable.Range(0, arrayCount));
             }
 
             return randomIndices;
