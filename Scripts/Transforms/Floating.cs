8fa789a0ca0fff8295326fa450d71e6b04a03e7c
Kevin <kevinvqin@gmail.com>
2020-04-29 22:13:12 +0800

    floating script

---
+++ Scripts/Transforms/Floating.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toolkit
+{
+    [AddComponentMenu("Toolkit/Transforms/Floating")]
+    public class Floating : MonoBehaviour
+    {
+        public float floatSpeed = 1f;
+        public float floatRange = 1f;
+        public float rotationSpeed = 20.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 localPosition = transform.localPosition;
+            Vector3 eulerAngles = transform.eulerAngles;
+
+            localPosition.y = Mathf.Sin(Time.time * floatSpeed) * floatRange;
+            eulerAngles.y += rotationSpeed * Time.deltaTime;
+
+            transform.localPosition = localPosition;
+            transform.eulerAngles = eulerAngles;
+        }
+    }
+}
