d5481b0b5966787a6f95f01ee44e036253b582d1
Kevin <kevinvqin@gmail.com>
2020-05-02 12:42:05 +0800

    add integration utility

--- Scripts/Utilities/DateUtility.cs
+++ Scripts/Utilities/DateUtility.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
 using UnityEngine;
 
 namespace Toolkit
