8ede82d4442ea35939178be6840f13519c05cd06
Kevin <kevinvqin@gmail.com>
2020-05-11 11:07:10 +0800

    add LabelFollow script

---
+++ Scripts/UI/LabelFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Toolkit
+{
+    [AddComponentMenu("Toolkit/UI/LabelFollow")]
+    public class LabelFollow : MonoBehaviour
+    {
+        public Transform target;
+        public Text text;
+        public Vector3 offset = new Vector3(0, 5, 0);
+
+        private Camera mainCamera;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 position = mainCamera.WorldToScreenPoint(target.position) + offset;
+            transform.position = new Vector3(Mathf.Clamp(position.x, 0, Screen.width), Mathf.Clamp(position.y, 0, Screen.height), position.z);
+            transform.rotation = Quaternion.FromToRotation(Vector3.down, position - transform.position);
+            text.transform.rotation = Quaternion.identity;
+        }
+    }
+}
