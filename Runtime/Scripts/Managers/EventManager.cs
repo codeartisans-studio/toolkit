fc6bc2d2e2c3b881e47bfb36f0c7681fbabafab5
Kevin <kevinvqin@gmail.com>
2020-11-03 11:13:30 +0800

    support object event

--- Runtime/Scripts/Managers/EventManager.cs
+++ Runtime/Scripts/Managers/EventManager.cs
@@ -9,48 +9,92 @@ namespace Toolkit
 
     public static class EventManager
     {
-        private static Dictionary<string, CustomEvent> eventDictionary = new Dictionary<string, CustomEvent>();
+        private static Dictionary<string, CustomEvent> GlobalEventDictionary = new Dictionary<string, CustomEvent>();
+        private static Dictionary<object, Dictionary<string, CustomEvent>> ObjectEventDictionary = new Dictionary<object, Dictionary<string, CustomEvent>>();
+
+        public static void AddListener(object obj, string eventName, UnityAction<string, object> listener)
+        {
+            Dictionary<string, CustomEvent> dictionary;
+            if (!ObjectEventDictionary.TryGetValue(obj, out dictionary))
+            {
+                dictionary = new Dictionary<string, CustomEvent>();
+                ObjectEventDictionary.Add(obj, dictionary);
+            }
+
+            CustomEvent thisEvent = null;
+            if (!dictionary.TryGetValue(eventName, out thisEvent))
+            {
+                thisEvent = new CustomEvent();
+                dictionary.Add(eventName, thisEvent);
+            }
+
+            thisEvent.AddListener(listener);
+        }
 
         public static void AddListener(string eventName, UnityAction<string, object> listener)
         {
             CustomEvent thisEvent = null;
-            if (eventDictionary.TryGetValue(eventName, out thisEvent))
-            {
-                thisEvent.AddListener(listener);
-            }
-            else
+            if (!GlobalEventDictionary.TryGetValue(eventName, out thisEvent))
             {
                 thisEvent = new CustomEvent();
-                thisEvent.AddListener(listener);
-                eventDictionary.Add(eventName, thisEvent);
+                GlobalEventDictionary.Add(eventName, thisEvent);
+            }
+
+            thisEvent.AddListener(listener);
+        }
+
+        public static void RemoveListener(object obj, string eventName, UnityAction<string, object> listener)
+        {
+            Dictionary<string, CustomEvent> dictionary;
+            if (ObjectEventDictionary.TryGetValue(obj, out dictionary))
+            {
+                CustomEvent thisEvent = null;
+                if (dictionary.TryGetValue(eventName, out thisEvent))
+                    thisEvent.RemoveListener(listener);
             }
         }
 
         public static void RemoveListener(string eventName, UnityAction<string, object> listener)
         {
             CustomEvent thisEvent = null;
-            if (eventDictionary.TryGetValue(eventName, out thisEvent))
-            {
+            if (GlobalEventDictionary.TryGetValue(eventName, out thisEvent))
                 thisEvent.RemoveListener(listener);
+        }
+
+        public static void RemoveAllListeners(object obj, string eventName)
+        {
+            Dictionary<string, CustomEvent> dictionary;
+            if (ObjectEventDictionary.TryGetValue(obj, out dictionary))
+            {
+                CustomEvent thisEvent = null;
+                if (dictionary.TryGetValue(eventName, out thisEvent))
+                    thisEvent.RemoveAllListeners();
             }
         }
 
         public static void RemoveAllListeners(string eventName)
         {
             CustomEvent thisEvent = null;
-            if (eventDictionary.TryGetValue(eventName, out thisEvent))
-            {
+            if (GlobalEventDictionary.TryGetValue(eventName, out thisEvent))
                 thisEvent.RemoveAllListeners();
+        }
+
+        public static void Invoke(object obj, string eventName, object eventParams)
+        {
+            Dictionary<string, CustomEvent> dictionary;
+            if (ObjectEventDictionary.TryGetValue(obj, out dictionary))
+            {
+                CustomEvent thisEvent = null;
+                if (GlobalEventDictionary.TryGetValue(eventName, out thisEvent))
+                    thisEvent.Invoke(eventName, eventParams);
             }
         }
 
         public static void Invoke(string eventName, object eventParams)
         {
             CustomEvent thisEvent = null;
-            if (eventDictionary.TryGetValue(eventName, out thisEvent))
-            {
+            if (GlobalEventDictionary.TryGetValue(eventName, out thisEvent))
                 thisEvent.Invoke(eventName, eventParams);
-            }
         }
     }
 }
