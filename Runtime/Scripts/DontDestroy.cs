63430c7ce79dd78db660f6348c6f7613be8ed000
Kevin <kevinvqin@gmail.com>
2020-11-12 18:37:38 +0800

    add dontdestroy script

---
+++ Runtime/Scripts/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
