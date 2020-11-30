753b23c90ac9b4006e5a33269b8127b34c84bc5e
Kevin <kevinvqin@gmail.com>
2020-11-30 12:24:37 +0800

    math utility

--- Runtime/Scripts/Utilities/MathUtility.cs
+++ Runtime/Scripts/Utilities/MathUtility.cs
@@ -7,6 +7,31 @@ namespace Toolkit
 {
     public static class MathUtility
     {
+        /// <summary>
+        /// Transforms the position from local space to world space. This is similar to Transform.TransformPoint but does not require a Transform.
+        /// </summary>
+        /// <param name="worldPosition">The world position of the object.</param>
+        /// <param name="rotation">The world rotation of the object.</param>
+        /// <param name="localPosition">The local position of the object</param>
+        /// <returns>The world space position.</returns>
+        public static Vector3 TransformPoint(Vector3 worldPosition, Quaternion rotation, Vector3 localPosition)
+        {
+            return worldPosition + (rotation * localPosition);
+        }
+
+        /// <summary>
+        /// Transforms the position from world space to local space. This is similar to Transform.InverseTransformPoint but does not require a Transform.
+        /// </summary>
+        /// <param name="worldPosition">The world position of the object.</param>
+        /// <param name="rotation">The world rotation of the object.</param>
+        /// <param name="position">The position of the object.</param>
+        /// <returns>The local space position.</returns>
+        public static Vector3 InverseTransformPoint(Vector3 worldPosition, Quaternion rotation, Vector3 position)
+        {
+            var diff = position - worldPosition;
+            return Quaternion.Inverse(rotation) * diff;
+        }
+
         // 一定数量元素随机放入数组
         // 如果需要数量大于数组数量，重新生成数组再次随机
         // 返回一个一定数量索引素组
